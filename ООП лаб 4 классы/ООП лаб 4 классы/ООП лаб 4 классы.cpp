afc78272b0b79730bd282ba110c52ba7c28a12ab
Sakhaya <96055547+Sakhaya@users.noreply.github.com>
2021-12-21 00:54:10 +1000

    исправленное

--- ООП лаб 4 классы/ООП лаб 4 классы/ООП лаб 4 классы.cpp
+++ ООП лаб 4 классы/ООП лаб 4 классы/ООП лаб 4 классы.cpp
@@ -3,41 +3,176 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdio> // printf
 using namespace std;
 
-class Apteca {
+class Pokyp {
 public:
-    string Pokyp;
-    string Farm;
-    string Resept;
-    string Lekarstvo;
+    string name;
+    int age;
 
-   //Обычный конструктор
-    Apteca(string P, string F, string R, string L) {
-        Pokyp = P;
-        Farm = F;
-        Resept = R;
-        Lekarstvo = L;
+    Pokyp() {
+        name = "";
+        age = 0;
     }
-    //Конструктор копий 
-    Apteca(const Apteca& o) {
-        Pokyp = o.Pokyp;
-        Farm = o.Farm;
-        Resept = o.Resept;
-        Lekarstvo = o.Lekarstvo;
+
+    Pokyp(string f,int i) {
+        name = f;
+        age = i;
     }
-    
+
+    ~Pokyp() {
+
+    }
 };
 
+class Farm {
+public:
+    string name;
+    int age;
+
+    Farm() {
+        name = "";
+        age = 0;
+    }
+
+    Farm(string f, int i) {
+        name = f;
+        age = i;
+    }
+
+    ~Farm() {
+
+    }
+};
+
+class Tovar {
+public:
+    string name;
+    int price;
+
+    Tovar() {
+        name = "";
+        price = 0;
+    }
+
+    Tovar(string f, int i) {
+        name = f;
+        price = i;
+    }
+
+    ~Tovar() {
+
+    }
+
+    void print() {
+        cout << " " << name << " " << price;
+    }
+};
+
+class Apteca {
+private:
+    
+public:
+    Pokyp kl;
+    Farm Rab;
+    vector<Tovar> Resept;
+    
+   //Обычный конструктор 
+    Apteca(const Pokyp&p,const Farm&W, vector<Tovar> Res)
+    {
+        kl = p;
+        Rab = W;
+        Resept = Res;
+        
+    }
+    //Конструктор копий 
+    Apteca(const Apteca& o){
+        kl = o.kl;
+        Rab = o.Rab;
+        Resept = o.Resept;
+        
+    }
+    friend void Spisok(Apteca);
+};
+
+void Spisok(Apteca){
+
+
+}
 int main()
 {
-    Apteca ap1("Erik","Vlad", "#456","Aspirin");
-    Apteca ap2("Alik", "Vlad", "#999", "Fruttella");
+
+    Pokyp p1("Erik", 54);
+    Farm f1("Vlad", 23);
+    Pokyp p2("Arina", 60);
+    Farm f2("Vlad", 53);
+    vector<Tovar> Res;
+    vector<Tovar> Spis;
+
+    Tovar t[10];
+    Tovar tg;
+    t[1].name = "Remandin";
+    t[1].price = 1;
+    t[2].name = "Aspirin";
+    t[2].price = 2;
+    t[3].name = "Vitamin";
+    t[3].price = 3;
+    t[4].name = "Nazivin";
+    t[4].price = 4;
+    t[5].name = "Noshpa";
+    t[5].price = 5;
+    t[6].name = "Likosol";
+    t[6].price = 6;
+    t[7].name = "Lazolvan";
+    t[7].price = 7;
+    t[8].name = "Akvamaris";
+    t[8].price = 8;
+    t[9].name = "Troksovazin";
+    t[9].price = 9;
+       
+       for (int k = 1; k < 10; k++) {
+       Res.push_back(t[k]);
+       Spis.push_back(t[k]);
+       }
+       
+       random_shuffle(Res.begin(), Res.end());
+       random_shuffle(Spis.begin(), Spis.end());
+
+
+
+    Apteca ap1(p1,f1, Res);
+    Apteca ap2(p2, f2, Spis);
     
-    cout <<"Begin ap1: "<< ap1.Pokyp << " " << ap1.Farm << " " << ap1.Resept << " " << ap1.Lekarstvo << endl;
-    cout << "Begin ap2: " << ap2.Pokyp << " " << ap2.Farm << " " << ap2.Resept << " " << ap2.Lekarstvo << endl;
+    cout << "Begin ap1: " << ap1.kl.name << " " << ap1.kl.age << " " << ap1.Rab.name << " " << ap1.Rab.age << " ";
+    for (vector<Tovar>::const_iterator it = ap1.Resept.begin(); it != ap1.Resept.end(); ++it) {
+ 
+        cout << (*it).name << " " << (*it).price << " ";
+    }
+    cout << endl;
+    cout << "Begin ap2: " << ap2.kl.name << " " << ap2.kl.age << " " << ap2.Rab.name << " " << ap2.Rab.age << " "; 
+    for (vector<Tovar>::const_iterator it = ap2.Resept.begin(); it != ap2.Resept.end(); ++it) {
+
+        cout << (*it).name << " " << (*it).price << " ";
+    }
+    cout << endl;
+
+
     ap1 = ap2;
-    cout << "End ap1: " << ap1.Pokyp << " " << ap1.Farm << " " << ap1.Resept << " " << ap1.Lekarstvo << endl;
-    cout << "End ap2: " << ap2.Pokyp << " " << ap2.Farm << " " << ap2.Resept << " " << ap2.Lekarstvo << endl;
-    
+
+    cout << endl;
+    cout << "End ap1: " << ap1.kl.name << " " << ap1.kl.age << " " << ap1.Rab.name << " " << ap1.Rab.age << " ";
+    for (vector<Tovar>::const_iterator it = ap1.Resept.begin(); it != ap1.Resept.end(); ++it) {
+
+        cout << (*it).name << " " << (*it).price << " ";
+    }
+    cout << endl;
+    cout << "End ap2: " << ap2.kl.name << " " << ap2.kl.age << " " << ap2.Rab.name << " " << ap2.Rab.age << " ";
+    for (vector<Tovar>::const_iterator it = ap2.Resept.begin(); it != ap2.Resept.end(); ++it) {
+
+        cout << (*it).name << " " << (*it).price << " ";
+    }
+    cout << endl;
 }
